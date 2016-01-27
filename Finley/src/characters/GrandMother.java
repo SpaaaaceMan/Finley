833ca485e67d4898d1e3353fc08c1ef12b4c88b7
Julien <julien7397@hotmail.fr>
2016-01-27 19:01:52 +0100

    nouveaux pers

---
+++ Finley/src/characters/GrandMother.java
@@ -0,0 +1,9 @@
+package characters;
+
+public class GrandMother extends Actor {
+
+	public GrandMother(int maxLife, int strength, int maxPower, double maxWeight) {
+		super("Grand-mère", maxLife, strength, maxPower, maxWeight);
+	}
+
+}
