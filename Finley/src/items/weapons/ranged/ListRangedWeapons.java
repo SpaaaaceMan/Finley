5fd934fc12cbce014513cbb123748f6bfd5d8571
Thomas <medard.thomas@outlook.fr>
2016-01-30 13:51:44 +0100

    Traduction nom des items

--- Finley/src/items/weapons/ranged/ListRangedWeapons.java
+++ Finley/src/items/weapons/ranged/ListRangedWeapons.java
@@ -6,7 +6,7 @@ public class ListRangedWeapons {
 	private static ArrayList<RangedWeapon> rangedWeapons = new ArrayList<RangedWeapon>();
 	
 	static {
-		rangedWeapons.add(new RangedWeapon("Bow", 5, 5, 60, 50));
+		rangedWeapons.add(new RangedWeapon("Arc", 5, 5, 60, 50));
 	}
 
 	public static RangedWeapon getRangedWeapons(int index) {
