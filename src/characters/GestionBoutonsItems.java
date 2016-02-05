896d2141fa811d461eb220f52d8b7e985af16b08
Julien <julien7397@hotmail.fr>
2016-02-05 16:00:29 +0100

    ajout des classes manquantes

---
+++ src/characters/GestionBoutonsItems.java
@@ -0,0 +1,103 @@
+package characters;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import items.Item;
+import items.potions.Potion;
+import items.weapons.Weapon;
+import items.weapons.ranged.RangedWeapon;
+import items.weapons.ranged.munitions.Munition;
+import items.wearables.Wearable;
+import items.wearables.armors.Armor;
+
+public class GestionBoutonsItems {
+	public static void initialiserListButtonItem(final Item item){
+		/*Uniquement les armes*/
+		if (item instanceof Weapon){
+			/*uniquement les armes à distance*/
+			if (item instanceof RangedWeapon){
+				JButton buttonReload = new JButton("Recharger");
+				item.getListMenuItems().add(buttonReload);
+				buttonReload.addActionListener(new ActionListener() {
+					 
+		            public void actionPerformed(ActionEvent e)
+		            {
+		            	//this.add(Munition munition);
+		            }
+		        }); 
+			}
+			if (item.getOwner().getWeapon() != item) {
+				JButton buttonEquip = new JButton("Equiper");
+				item.getListMenuItems().add(buttonEquip);
+				buttonEquip.addActionListener(new ActionListener() {
+					 
+		            public void actionPerformed(ActionEvent e)
+		            {
+		            	item.use(item.getOwner());
+		            }
+		        });
+			}
+			else {
+				JButton buttonUnEquip = new JButton("Deséquiper");
+				item.getListMenuItems().add(buttonUnEquip);
+				buttonUnEquip.addActionListener(new ActionListener() {
+					 
+		            public void actionPerformed(ActionEvent e)
+		            {
+		            	item.getOwner().setWeapon(null);
+		            	System.out.println(item.getOwner().getName() + " se déséquipe de " + item.getName());
+		            }
+		        });
+			}
+		}
+		/*uniquement les armures*/
+		else if (item instanceof Armor){
+			JButton buttonAddTo = new JButton("se dénuder");
+			item.getListMenuItems().add(buttonAddTo);
+			buttonAddTo.addActionListener(new ActionListener() {
+				 
+	            public void actionPerformed(ActionEvent e)
+	            {
+	            	//lol
+	            }
+	        }); 
+		}
+		/*uniquement les munitions*/
+		else if (item instanceof Munition){
+			JButton buttonAddTo = new JButton("Ajouter à");
+			item.getListMenuItems().add(buttonAddTo);
+			buttonAddTo.addActionListener(new ActionListener() {
+				 
+	            public void actionPerformed(ActionEvent e)
+	            {
+	            	//RangedWeapon.addMunition(this);
+	            }
+	        }); 
+		}
+		/*uniquement les potions*/
+		else if (item instanceof Potion){
+			JButton buttonDrink = new JButton("Boire");
+			item.getListMenuItems().add(buttonDrink);
+			buttonDrink.addActionListener(new ActionListener() {
+				 
+	            public void actionPerformed(ActionEvent e)
+	            {
+	            	item.use(item.getOwner());
+	            }
+	        }); 
+		}
+		/*tous les items*/
+		JButton buttonDrop = new JButton("Lâcher");
+		item.getListMenuItems().add(buttonDrop);
+		buttonDrop.addActionListener(new ActionListener() {
+			 
+            public void actionPerformed(ActionEvent e)
+            {
+            	item.getOwner().dropItem(item);
+            }
+        }); 
+	}
+}
---
+++ src/ihm/ModeleDynamiqueObjet.java
@@ -0,0 +1,92 @@
+package ihm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.JTableHeader;
+
+import items.Item;
+
+@SuppressWarnings("serial")
+public class ModeleDynamiqueObjet extends AbstractTableModel {
+    private final List<Item> items = new ArrayList<Item>();
+    private final List<Integer> quantities = new ArrayList<Integer>();
+ 
+    private final String[] entetes = {"Icône", "Nom", "Poids", "Valeur", "Place", "Quantité"};
+ 
+    public ModeleDynamiqueObjet() {
+        super();
+    }
+ 
+    public int getRowCount() {
+        return items.size();
+    }
+ 
+    public int getColumnCount() {
+        return entetes.length;
+    }
+ 
+    public String getColumnName(int columnIndex) {
+        return entetes[columnIndex];
+    }
+    
+    public Class getColumnClass(int column)
+    {
+    	if (items.isEmpty())
+            return Object.class;
+        return getValueAt(0, column).getClass();
+    }
+ 
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch(columnIndex){
+        	case 0:
+        		return items.get(rowIndex).getIcon();
+            case 1:
+                return items.get(rowIndex).getName();
+            case 2:
+                return items.get(rowIndex).getWeight();
+            case 3:
+                return items.get(rowIndex).getValue();
+            case 4:
+                return items.get(rowIndex).getPlaceOccupiedInventory();
+            case 5:
+            	return quantities.get(rowIndex);
+            default:
+                return null; //Ne devrait jamais arriver
+        }
+    }
+ 
+    public void addItem(Item item) {
+        items.add(item);
+        quantities.add(1);
+ 
+        fireTableRowsInserted(items.size() -1, items.size() -1);
+    }
+ 
+    public void removeItem(int rowIndex) {
+        if (quantities.get(rowIndex) == 1){
+        	items.remove(rowIndex);
+        	quantities.remove(rowIndex);
+        }
+        else
+        	quantities.set(rowIndex, quantities.get(rowIndex) - 1);
+ 
+        fireTableRowsDeleted(rowIndex, rowIndex);
+    }
+
+	@Override
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		if (columnIndex == 5)
+			quantities.set(rowIndex, (Integer) aValue);
+		else
+			super.setValueAt(aValue, rowIndex, columnIndex);
+	}
+
+	public List<Item> getItems() {
+		return items;
+	} 
+}
+
