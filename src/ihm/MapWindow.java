94fff551237ed323e41a2424d6ff305edd7cc98d
Lucie <lucie.pellottiero@gmail.com>
2016-02-06 13:40:35 +0100

    Ajout des coordonnées du héro

--- src/ihm/MapWindow.java
+++ src/ihm/MapWindow.java
@@ -1,14 +1,18 @@
 package ihm;
 
+import java.awt.BasicStroke;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.StrokeBorder;
 
 import characters.Actor;
 import maps.Map;
@@ -23,12 +27,21 @@ public class MapWindow extends JFrame implements Observer {
 	
 	/**La {@link maps.Map} de cette fenetre.
 	 */
-	@SuppressWarnings("unused")
 	private Map map;
 	
 	/**Le {@link JPanel} representant la {@link maps.Map}.
 	 */
 	private JPanel mapPanel;
+	
+	/**
+	 * les coordonnées en X de l'{@link #hero}.
+	 */
+	private int heroX = 0;
+	
+	/**
+	 * les coordonnées en Y de l'{@link #hero}.
+	 */
+	private int heroY = 0;
 	
 	/**Vous! (Votre {@link Actor}).
 	 * 
@@ -40,9 +53,11 @@ public class MapWindow extends JFrame implements Observer {
 		super("Map");
 		
 		//Initialisation des attributs
-		this.hero 	  = hero;
-		this.map 	  = map;
-		this.mapPanel = new JPanel(new GridLayout(16, 16));
+		this.hero 	   = hero;
+		this.map 	   = map;
+		this.mapPanel  = new JPanel(new GridLayout(16, 16));
+		
+		//initialisation des composants static
 		
 		//Ajout en tant qu'observer
 		this.hero.addObserver(this);
@@ -51,9 +66,18 @@ public class MapWindow extends JFrame implements Observer {
 		//Création d'une map simple.
 		for (ArrayList<Place> row : map.getMap()) {
 			for (Place column : row) {
-				this.mapPanel.add(new JLabel ("" + column.getType().charAt(0)));
+				JLabel mapLabel = new JLabel ("" + column.getType().charAt(0));
+				this.mapPanel.add(mapLabel);
 			}
 		}
+		
+		//Indication de la position du héro
+		JLabel mapLabel = (JLabel) mapPanel.getComponent(10 * heroX + heroY);
+		mapLabel.setBackground(Color.RED);
+		mapLabel.setOpaque(true);
+		
+		//Ajout d'une Border au mapPanel
+		mapPanel.setBorder(BorderFactory.createStrokeBorder(new BasicStroke(3)));
 		
 		//Ajout des composants et du Layout
 		this.setLayout(new BorderLayout());
@@ -66,8 +90,6 @@ public class MapWindow extends JFrame implements Observer {
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
